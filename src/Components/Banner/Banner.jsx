971d93029351330850dd572ebff5030161ae58fc
Heli8282 <helijorgefacundo@gmail.com>
2025-11-12 11:47:00 -0300

    Header e Cart Concluídos

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,79 +1,98 @@
+import "./Banner.css"
+import shoeImage from "../../assets/shoeImage.png"
+
 function Banner() {
   return (
     <>
-      <section>
+      <section className="cart-section">
 
-        <div>
+        <div className="div-cart">
 
-          <div>
-            <h6>MEU CARRINHO</h6>
-            <h6>QUANTIDADE</h6>
-            <h6>UNITÁRIO</h6>
-            <h6>TOTAL</h6>
+          <div className="my-cart">
+            <span>MEU CARRINHO</span>
+            <span>QUANTIDADE</span>
+            <span>UNITÁRIO</span>
+            <span>TOTAL</span>
           </div>
 
-          <div>
-            <img src="" alt="" />
-            <div>
+          <div className="products">
+            <img src={shoeImage} alt="Tênis Nike" />
+
+            <div className="product-description">
               <p>Tênis Nike Revolution 6 Next Nature Masculino</p>
               <p>Cor: Vermelho / Branco</p>
               <p>Tamanho: 42</p>
             </div>
-            <div>
-              <div class="contador">
-                <button id="menos">−</button>
-                <span id="numero">0</span>
-                <button id="mais">+</button>
-                <a href="#" id="remover">Remover Ítem</a>
+
+            <div className="product-counter">
+              <div>
+                <button id="btn-less">−</button>
+                <span id="value">1</span>
+                <button id="btn-more">+</button>
               </div>
+              <a href="#" id="remove-item">Remover Ítem</a>
             </div>
-            <div>
-              <p>R$249,00 R$249,00</p>
-              <p>R$219,00 R$219,00</p>
+
+            <div className="price">
+              <p className="old-price">
+                <span>R$249,00 </span>
+                <span>R$249,00</span>
+              </p>
+              <p className="current-price">
+                <span>R$219,00</span>
+                <span>R$219,00</span>
+              </p>
             </div>
+
           </div>
 
-          <div>
-            <div>
+          <div className="transport-discount">
+            <div className="discount-section">
               <h6>Cupom de desconto</h6>
-              <input type="text" placeholder="Insira seu código"/>
+              <input type="text" placeholder="Insira seu código" />
               <button>OK</button>
             </div>
-            <div>
+
+            <div className="shipping-section">
               <h6>Calcular frete</h6>
-              <input type="text" placeholder="Insira seu CEP"/>
+              <input type="text" placeholder="Insira seu CEP" />
               <button>OK</button>
             </div>
           </div>
 
         </div>
 
-        <div>
-          <div>
-            <h6>RESUMO</h6>
-          </div>
-          <div>
-            <p>Subtotal: R$219,00</p>
-          </div>
-          <div>
-            <p>Frete: R$0,00</p>
-          </div>
-          <div>
-            <p>Desconto: R$30,00</p>
-          </div>
-          <div>
-            <p>Total: R$189,00</p>
-          </div>
-          <div>
-            <p>ou 10x de 24,90 sem juros</p>
-          </div>
+        <div className="div-summary">
+          <p>RESUMO</p>
+
+          <p className="subtotal">
+            <span id="font">Subtotal:</span><span>R$249,00</span>
+          </p>
+
+          <p className="subtotal">
+            <span id="font">Frete:</span><span>R$0,00</span>
+          </p>
+
+          <p className="subtotal">
+            <span id="font">Desconto:</span><span>R$30,00</span>
+          </p>
+
+          <p className="subtotal">
+            <span id="total">Total:</span><span id="total-value">R$219,00</span>
+          </p>
+
+          <p className="part">
+            ou 10x de 24,90 sem juros
+          </p>
+
+          <button id="orange-button">Continuar</button>
         </div>
-        <button id="meu-botao">Continuar</button>
+
 
       </section>
 
     </>
-  )
+  );
 }
 
 export default Banner
