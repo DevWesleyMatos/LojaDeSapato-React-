3176f72f4906e3e7bd26e151e6742f82e20228b6
DevWesleyMatos <wesley.matos.ti@gmail.com>
2025-11-13 21:09:42 -0300

    correções

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import ProductsPages from "./Pages/ProductsPages";
 
 function App() {
-  
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/Products" element={<ProductsPages/>} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter basename="/LojaDeSapato-React-/">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Products" element={<ProductsPages />} />
+      </Routes>
+    </BrowserRouter>
   );
-
 }
 
-export default App
+export default App;
