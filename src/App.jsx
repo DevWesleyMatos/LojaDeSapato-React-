b6e2be8378df4325735a234c33a8a2b46e48991e
DevWesleyMatos <wesley.matos.ti@gmail.com>
2025-11-22 06:24:48 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AdministrationProduct from "./Pages/AdministrationProduct";
 
 function App() {
   return (
-    <BrowserRouter basename="/LojaDeSapato-React-/">
+    <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/AdministrationProduct" element={<AdministrationProduct />} />
