5c9b225220168778b964d001d8bd2c7d6f0f8ea5
DevWesleyMatos <wesley.matos.ti@gmail.com>
2025-11-22 06:19:46 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/LojaDeSapato-React-/"
-})
+  base: process.env.VITE_BASE_URL || '/',
+}))
